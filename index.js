fd0de8e7668d31ab3ca250bcd4cf29cae8901c8d
F2Ville <feliciendeville@gmail.com>
2023-10-19 20:54:28 +0200

    Editing CORS

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const express = require("express"),
     fs = require("fs"),
     port = process.env.PORT || 3000
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    next();
+});
+
 app.set("view engine", "pug")
 app.use("/", express.static("./public"))
 
